d03ad64a96ba610a8fa564a9f81e41bb6f2beb79
Hemulus <sfrankford@gmail.com>
2025-02-15 23:28:11 -0600

    godot-3-adjusting-projectile-inertia

--- multiplayer_fps/projectile.gd
+++ multiplayer_fps/projectile.gd
@@ -4,27 +4,27 @@ extends RigidBody3D
 @export var gravity: float = -9.8
 
 func _ready() -> void:
-    add_to_group("projectiles")
-    gravity_scale = 0.5
-    mass = 0.26
-    can_sleep = true
-    # Enable continuous collision detection
-    contact_monitor = true
-    max_contacts_reported = 4
-    continuous_cd = true
+	add_to_group("projectiles")
+	gravity_scale = 0.5
+	mass = 0.26
+	can_sleep = true
+	# Enable continuous collision detection
+	contact_monitor = true
+	max_contacts_reported = 4
+	continuous_cd = true
 
 func _physics_process(_delta: float) -> void:
-    # Optional: Delete projectile if it goes too far below the ground
-    if global_position.y < -50:
-        queue_free()
-        print("Projectile deleted for going too far below the ground.")
-    # Optional: Delete projectile if it's nearly stationary
-    #if linear_velocity.length() < 0.1:
-    #    queue_free()
-    #    print("Projectile deleted due to low velocity.")
+	# Optional: Delete projectile if it goes too far below the ground
+	if global_position.y < -50:
+		queue_free()
+		print("Projectile deleted for going too far below the ground.")
+	# Optional: Delete projectile if it's nearly stationary
+	#if linear_velocity.length() < 0.1:
+	#    queue_free()
+	#    print("Projectile deleted due to low velocity.")
 
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
-    var velocity = state.linear_velocity
-    velocity.y += gravity * state.step
-    state.linear_velocity = velocity
+	var velocity = state.linear_velocity
+	velocity.y += gravity * state.step
+	state.linear_velocity = velocity
